2f96142db4306f8a9c6a545b81cffd3652b08af0
Tony Michael <tony.correia19@outlook.com>
2019-11-01 02:34:20 -0300

    Add files via upload

---
+++ TestesAPI/src/main/java/br/com/estudandoapi/HelloWorld.java
@@ -0,0 +1,15 @@
+package br.com.estudandoapi;
+
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+		System.out.println(response.getBody().asString());
+		System.out.println(response.statusCode());
+	}
+}
---
+++ TestesAPI/src/main/java/br/com/estudandoapi/PrimeiroTeste.java
@@ -0,0 +1,11 @@
+package br.com.estudandoapi;
+
+import org.junit.Test;
+
+public class PrimeiroTeste {
+
+	@Test
+	public void testOlaMundo(){
+		
+	}
+}
